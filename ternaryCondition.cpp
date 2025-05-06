750efbdf8d19f54e0efc438cfba3ae0159c0c2e5
Md Rony Islam <mdronyislam149@gmail.com>
2025-05-06 21:24:15 +0600

    new things are added

---
+++ ternaryCondition.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+int main()
+{
+    int n=-3;
+    cout <<(n>= 0 ? "positive": "negative");
+    return 0;
+}           
---
+++ type_casting.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+int main()
+{
+    double price = 99.99;
+    int newValue = (int) price;
+    cout << newValue <<endl;
+    return 0;
+}
